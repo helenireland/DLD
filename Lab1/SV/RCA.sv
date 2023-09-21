8a4da477993c6c7226bb2962b4dfb979dfb74e3a
Wright <helen.i.wright@okstate.edu>
2023-09-21 15:14:58 -0500

    Lab1Finish

--- Lab1/SV/RCA.sv
+++ Lab1/SV/RCA.sv
@@ -1,4 +1,5 @@
-module RCA (input logic [3:0] a, b, input logic c, output logic [3:0] sum, output logic f);
+
+module RCA (input logic [3:0] a, b, input logic c, output logic [3:0] y, output logic [3:0] sum);
   logic [2:0] cin;
   
   
@@ -7,5 +8,5 @@ module RCA (input logic [3:0] a, b, input logic c, output logic [3:0] sum, outpu
   silly FA0 (a[0], b[0], c, cin[0], sum[0]); 
   silly FA1 (a[1], b[1], cin[0], cin[1], sum[1]);
   silly FA2 (a[2], b[2], cin[1], cin[2], sum[2]);
-  silly FA3 (a[3], b[3], cin[2], f, sum[3]);
+  silly FA3 (a[3], b[3], cin[2], y[0], sum[3]);
 endmodule
--- Lab1/SV/RCA_tb.sv
+++ Lab1/SV/RCA_tb.sv
@@ -9,33 +9,47 @@ logic [3:0]       a;
    logic [3:0] 	b;
    logic 	c;
    
-   logic [3:0] f;
+   logic [3:0] y;
    logic [3:0] sum;
    logic [2:0] cin;
-   logic        clk;   
+   logic        clk;
+   logic [3:0] Sum_correct;
+   logic [3:0] Error;   
 
-RCA dut(a, b, c, sum, f);
+RCA dut(a, b, c, y, sum);
 initial
 	begin
-		handle3 = $fopen("rca.out");
-		desc3 = handle3;
-		#1250 $finish;
+		
+		desc3 = $fopen("rca.out");
+		
+		#1900 $finish;
 	end
+	assign Sum_correct = a + b + c;
+		assign Error = y != (a & b) | (a & c) | (b & c);
+
+initial begin
+	clk = 1'b1;
+	forever #5 clk = ~clk;
+end
 
 initial
 begin
-	for (i=0; i < 128; i=i+1)
+	for (i=0; i < 180; i=i+1)
 	begin
 		// Put vectors before beginning of clk
+		
 		@(posedge clk)
 		begin
 			a = $random;
 			b = $random;
 			c = $random;
+			
+			
 		end
 		@(negedge clk)
 		begin
-			$fdisplay(desc3, "%h %h || %h | %h | %b", a, b, c, sum, (c == sum));
+
+			$fdisplay(desc3, "%h %h || %h | %h | %b", a, b, c, sum, (Sum_correct == sum));
 		end
 	end // @(negedge clk)
 end
